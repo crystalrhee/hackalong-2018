824244d9644b3ad17da903edad8e08332939c38d
Ivan Chen <ivan.if.chen@gmail.com>
2018-08-11 17:34:13 -0400

    turn it into a func

--- scripts/textToDict.py
+++ scripts/textToDict.py
@@ -5,19 +5,23 @@ import csv
 INPUT_FILE = 'test-dataset.csv'
 OUT_FILE = "out.csv"
 
+def textToDict(text):
+    vector = {}
+    for word in text.translate(string.punctuation) \
+                    .replace('\n', '') \
+                    .split(' '):
+        vector[word] = vector[word] + 1 if word in vector else 0
+    return vector
+
+
 with open(INPUT_FILE, 'r') as f:
     with open(OUT_FILE, 'w') as outfile:
         reader = csv.reader(f, quotechar='"')
         writer = csv.writer(outfile)
         for row in reader:
-            vector = {}
             try:
                 url, text = row
             except Exception as e:
                 print('bad csv format')
                 raise e
-            for word in text.translate(string.punctuation) \
-                            .replace('\n', '') \
-                            .split(' '):
-                vector[word] = vector[word] + 1 if word in vector else 0
-            writer.writerow([url, json.dumps(vector)])
+            writer.writerow([url, json.dumps(textToDict(text))])
