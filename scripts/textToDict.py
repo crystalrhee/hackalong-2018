983545a64282ba6600f11b96adf5585305fd0342
Ivan Chen <ivan.if.chen@gmail.com>
2018-08-11 16:50:19 -0400

    sctips folder and text to dict script

---
+++ scripts/textToDict.py
@@ -0,0 +1,23 @@
+import json
+import string
+import csv
+
+INPUT_FILE = 'test-dataset.csv'
+OUT_FILE = 'out.csv'
+
+with open(INPUT_FILE, 'r') as f:
+    with open(OUT_FILE, 'w') as outfile:
+        reader = csv.reader(f, quotechar='"')
+        writer = csv.writer(outfile)
+        for row in reader:
+            vector = {}
+            try:
+                url, text = row
+            except Exception as e:
+                print('bad csv format')
+                raise e
+            for word in text.translate(None, string.punctuation) \
+                            .replace('\n', '') \
+                            .split(' '):
+                vector[word] = vector[word] + 1 if word in vector else 0
+            writer.writerow([url, vector])
