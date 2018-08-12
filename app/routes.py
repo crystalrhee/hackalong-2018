a131a375c8482cb81d25f2d4174c43bfafd6c613
Crystal Rhee <crystal.rhee@gmail.com>
2018-08-12 02:12:21 -0400

    results page view

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
 from flask import Flask, request, render_template
 from app import app
+import json
 
 @app.route('/')
 @app.route('/index', methods=['POST'])
 def index():
-    user = {'username': 'Hackalong'}
-    return render_template('index.html', title='Home', user=user)
+	# if form.validate():
+	# 	return redirect(url_for('results'))
+	# else:
+	return render_template('index.html')
+
+def get_repos():
+	repos = [{"name": "repo1", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
+	{"name": "repo2", "description": "some description", "last_updated": 3, "link": "https://github.com/"},
+	{"name": "repo3", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
+	{"name": "repo4", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
+	{"name": "repo5", "description": "some description", "last_updated": 3, "link": "https://github.com/"},
+	{"name": "repo6", "description": "some description", "last_updated": 3, "link": "https://github.com/"}]
+	return repos
+
+@app.route('/results')
+def hello_world():
+	repos = get_repos()
+	return render_template("results.html", repos=(repos)) 
