ac6028431f4c1de80fa720ef7de42c0e3e6a4ff4
Crystal Rhee <crystal.rhee@gmail.com>
2018-08-12 00:58:40 -0400

    loading

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['POST'])
 def index():
     user = {'username': 'Hackalong'}
     return render_template('index.html', title='Home', user=user)
