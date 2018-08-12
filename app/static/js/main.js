ba341facc890b16050879e28dff04ba0f9e20f96
Crystal Rhee <crystal.rhee@gmail.com>
2018-08-11 22:07:11 -0400

    interface update

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,27 @@
-$(document).ready(function() {
-    $('#github_url').on('change', function() {
-        var value = $(this).val();
-        console.log('Selected Value: ', value);
-    });
+function checkStatus() {
+	$('#github_url').on('click', function() {
+	    $('.field-box').addClass('focus'); 
+	});
+	$('#github_url').focusin(function() {
+	    $('.field-box').addClass('focus'); 
+	});
+	$(document).on('click', function(e) {
+		if ($(e.target).is('#github_url') === false) {
+			$('.field-box').removeClass('focus'); 
+			checkForInput();
+		}
+	});
+}
 
+function checkForInput() {
+    if ($('#github_url').val().length > 0) {
+        $('.field-box').addClass('focus');
+    } else {
+        $('.field-box').removeClass('focus');
+    }
+}
+
+$('#github_url').on('change keyup', function() {
+    checkStatus();
 });
+
