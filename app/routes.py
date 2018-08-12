be4493be0af2d3d7b065ffd8d14fd602b2774ded
Crystal Rhee <crystal.rhee@gmail.com>
2018-08-12 03:00:46 -0400

    connect backend

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from app import app
-import json
+from scripts import gj
 
 @app.route('/')
 @app.route('/index', methods=['POST'])
@@ -10,16 +10,7 @@ def index():
 	# else:
 	return render_template('index.html')
 
-def get_repos():
-	repos = [{"name": "repo1", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
-	{"name": "repo2", "description": "some description", "last_updated": 3, "link": "https://github.com/"},
-	{"name": "repo3", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
-	{"name": "repo4", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "last_updated": 3, "link": "https://github.com/"},
-	{"name": "repo5", "description": "some description", "last_updated": 3, "link": "https://github.com/"},
-	{"name": "repo6", "description": "some description", "last_updated": 3, "link": "https://github.com/"}]
-	return repos
-
 @app.route('/results')
 def hello_world():
-	repos = get_repos()
+	repos = gj.get_repos()
 	return render_template("results.html", repos=(repos)) 
---
+++ app/scripts/__init__.py
@@ -0,0 +1,3 @@
+import get_json as gj
+
+gj.get_repos()
