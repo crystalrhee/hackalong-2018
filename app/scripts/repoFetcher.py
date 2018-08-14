8c2f267e07e950f6dc6a5b47b06567bbe954760a
Ivan Chen <ivan.if.chen@gmail.com>
2018-08-14 11:23:15 -0400

    added some more comments and better error handling

--- app/scripts/repoFetcher.py
+++ app/scripts/repoFetcher.py
@@ -32,35 +32,49 @@ def runMultiple(repo_urls, outfile, threads=2):
 
 if __name__ == '__main__':
     with open(config['output'], 'a') as outfile:
-        # state keeps track of the current/previous repo ID (begins from 1)
-        with open(config['state'], 'r') as state:
-            s = state.read()
-            if s:
-                prev_id = int(s)
         writer = csv.writer(outfile)
-        # Each iteration will fetch about 364 readmes because each api request returns that much (by defaut)
-        while True:
-            try:
-                # using an access token allows to make 5000 requests per hour (2018/08/14)
-                url = 'https://api.github.com/repositories?access_token={}&since={}'.format(config['token'], prev_id)
-                with request.urlopen(url) as response:
-                    print('====== downloading', prev_id, '======')
-                    page = response.read().decode('utf-8')
-                    repo_links = []
-                    for o in json.loads(page):
-                        html = o['html_url']
-                        repo_links.append(html)
-                        prev_id = o['id']
-                    try:
-                        runMultiple(repo_links, outfile, 12)
-                        # stores the state every ~364 repos being downloaded
-                        # Note: this implementation will not re-download each failed download or check for duplicates,
-                        #       it will simply resume at the previous N * 364'th ID and potentially have duplicates in the output
-                        with open(config['state'], 'w') as state:
-                            state.write(str(prev_id))
-                    except Exception as e:
-                        print(e)
-                        pass
-            except Exception as e:
-                print(e)
-                pass
+        # state keeps track of the current/previous repo ID (begins from 1)
+        try:
+            # found the previous state/id, use that
+            state = open(config['state'], 'r')
+            s = state.read()
+            prev_id = int(s) if s else 1
+        except:
+            # file doesn't exist or bad state content, create a new state
+            state = open(config['state'], 'w')
+            prev_id = 1
+        try:
+            failure_count = 0
+            # Each iteration will fetch about 364 readmes because each api request returns that much (by defaut)
+            while True:
+                try:
+                    # using an access token allows to make 5000 requests per hour (2018/08/14)
+                    url = 'https://api.github.com/repositories?access_token={}&since={}'.format(config['token'], prev_id)
+                    with request.urlopen(url) as response:
+                        print('====== downloading', prev_id, '======')
+                        page = response.read().decode('utf-8')
+                        repo_links = []
+                        for o in json.loads(page):
+                            html = o['html_url']
+                            repo_links.append(html)
+                            prev_id = o['id']
+                        try:
+                            runMultiple(repo_links, outfile, 12)
+                            # stores the state every ~364 repos being downloaded
+                            # Note: this implementation will not re-download each failed download or check for duplicates,
+                            #       it will simply resume at the previous N * 364'th ID and potentially have duplicates in the output
+                            with open(config['state'], 'w') as state:
+                                state.write(str(prev_id))
+                        except Exception as e:
+                            print('abc')
+                            print(e)
+                            pass
+                except Exception as e:
+                    failure_count += 1
+                    print(e)
+                    if failure_count >= 20:
+                        print('Too many failures, gonna give up now, kthxbai')
+                        exit()
+                    pass
+        finally:
+            state.close()
